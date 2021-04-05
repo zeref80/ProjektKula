bdfab5d5f958b299920a486e17e2358b34c07a11
zeref80 <45498824+zeref80@users.noreply.github.com>
2021-04-05 21:28:20 +0200

    Magic Circle p1

---
+++ ProjektKula/Assets/mass.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mass : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Debug.Log(rb.mass);
+    }
+}
+
+    // Update is called once per frame
+
+
